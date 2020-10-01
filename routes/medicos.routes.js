b98c12551de43353d2f72874a1183c9ede788044
Lidia Real <lidiarq96@gmail.com>
2020-10-01 11:26:36 +0200

    Fin sección 17

--- routes/medicos.routes.js
+++ routes/medicos.routes.js
@@ -14,11 +14,12 @@ const {
     crearMedico,
     actualizarMedico,
     borrarMedico,
+    getMedicoById
 } = require('../controllers/medicos');
 
 const router = Router();
 
-router.get('/', getMedicos);
+router.get('/', validarJWT, getMedicos);
 
 router.post('/', [
         validarJWT,
@@ -37,6 +38,6 @@ router.put('/:id', [
 
 router.delete('/:id', validarJWT, borrarMedico);
 
-
+router.get('/:id', validarJWT, getMedicoById);
 
 module.exports = router;
